10829f5ba2a331ad98f21808c917f7c11fb43b56
timbertinker <forest310125@gmail.com>
2015-01-17 10:45:17 -0600

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+Jan 2015 class on JavaScript
+
+Can use http://repl.it/landing to practice your JavaScript
+
+OR
+
+Use the console in the browser
---
+++ index.js
@@ -0,0 +1,78 @@
+// variables- store values
+// 1) declaration - delclare the var
+// 2) initialize
+
+// Ex1) 
+// delclaire
+// var x;
+// initialize
+// x = 5;
+// console.log(x);
+
+// Ex2)
+// both delclaires and initializes
+// var y = 2;
+// console.log(y);
+
+// Data Types
+// strings: an immutable string of characters.
+// var greeting = 'Hello';
+// var resturant = "chipoltle";
+
+// number: whole (6, -102) or floating point (5.467)
+// var myAge = 25;
+// var pi = 3.14;
+
+// boolean: represents logical true or false
+// var catsAreBest = false;
+// var dogsRule = true;
+
+// undefined: Represents a value that hasn't been defined.
+// var notDefinedYet;
+
+// null: represents ane empty value, purposely empty
+// var goodJokes = null;
+
+// variables have names, thats how you refer to them.
+// console.log(x);
+
+// Few 'rules'
+// Only contain letters, numbers, $ and _
+// Prefer camelCase for multiple Words (insead of under_score)
+// variables CANNOT start with a number!
+
+// Cool.
+// var numPeople, $mainHeader, _num, _Num;
+
+// Not Cool
+// var 2coolForSchool;
+
+// Reserved words.
+// Certain words are reserved byt he language and cannot be used by you.
+// A full list can be found online.
+// Ex) developer.mozilla.org
+
+// Operators
+// Allow you to manipulate variables, many are ones you already know. 
+// Mathematical operators:
+// + - * / =
+
+// Ex)
+// 2 + 2;
+// x * 3;
+
+// Variables can slo store results of these expressions.
+
+// Ex)
+// var x = 2 + 2;
+// console.log(x);
+
+// Strings - concatination
+
+// Ex)
+// var name = "Emma";
+// var greet = "Hello";
+// console.log(greet + " " + name);
+
+// JavaScript does not require a ';' at the end of statments, BUT it's a really good practice.
+
