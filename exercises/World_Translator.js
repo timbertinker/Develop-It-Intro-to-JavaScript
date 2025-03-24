40a23100e8252adb764bb7f9b88991f415ee2edd
FOREST <forest310125@gmail.com>
2025-03-23 18:33:33 -0600

    Update World_Translator.js

--- exercises/World_Translator.js
+++ exercises/World_Translator.js
@@ -6,7 +6,6 @@
 // takes 1 argument, a language code (e.g. "es", "de", "en")
 // returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
 // Call that function for each of the supported languages and log the result to make sure it works.
-
 function helloWorld(language) {
 	if (language == 'English'){
 		console.log("Hello World");
